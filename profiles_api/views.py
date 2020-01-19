c51f0b1a6fe4ed8bf877e3b2cb2db649cd818e8e
kamruljaman26 <kamrul.jaman26@gmail.com>
2020-01-19 20:07:44 +0600

    Project Done: API Added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
 # Create your views here.
+from profiles_api import serializers
+from profiles_api import models
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+# Create your views here.
+
+class LiveTvViewSet(viewsets.ModelViewSet):
+    """Handle createing, reading and updating LiveTv API Data"""
+    serializer_class = serializers.TvLinkSerializer
+    queryset = models.TvLink.objects.all()
+    #http_method_names allows only defined http method
+    http_method_names = ['get']
+
+class AdserviceViewSet(viewsets.ModelViewSet):
+    """Handle createing, reading and updating Ads Service"""
+    serializer_class = serializers.AdsServiceSerializer
+    queryset = models.AdsService.objects.all()
+    #http_method_names allows only defined http method
+    http_method_names = ['get']
+
+    def list(self, request):
+        serializer = serializers.AdsServiceSerializer(self.queryset, many=True)
+        return Response(serializer.data)
