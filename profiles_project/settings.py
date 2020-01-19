91d2cac04a252379616f391c95ba1a409db88af0
kamruljaman26 <kamrul.jaman26@gmail.com>
2020-01-19 20:34:02 +0600

    Config for server

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x3mpcz_-o%xon&(wd2f^2q+sk%)3cy1^$8f(b6pp5tab0w-acz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
