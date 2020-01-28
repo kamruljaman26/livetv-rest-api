4efff3de55bef450211aa26840ee93139ae0f56a
kamruljaman26 <kamrul.jaman26@gmail.com>
2020-01-28 18:00:06 +0600

    Fixed - Null Value Problem

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 class TvLink(models.Model):
     """Database models for Live TV App"""
-    tv1name = models.CharField(max_length=300,null=True, blank=True)
-    tv1link = models.CharField(max_length=300,null=True, blank=True)
-    tv1ylink = models.CharField(max_length=300,null=True, blank=True)
+    tv1name = models.CharField(max_length=300, blank=True)
+    tv1link = models.CharField(max_length=300, blank=True)
+    tv1ylink = models.CharField(max_length=300, blank=True)
 
-    tv2name = models.CharField(max_length=300,null=True, blank=True)
-    tv2link = models.CharField(max_length=300,null=True, blank=True)
-    tv2ylink = models.CharField(max_length=300,null=True, blank=True)
+    tv2name = models.CharField(max_length=300, blank=True)
+    tv2link = models.CharField(max_length=300, blank=True)
+    tv2ylink = models.CharField(max_length=300, blank=True)
 
-    tv3name = models.CharField(max_length=300,null=True, blank=True)
-    tv3link = models.CharField(max_length=300,null=True, blank=True)
-    tv3ylink = models.CharField(max_length=300,null=True, blank=True)
+    tv3name = models.CharField(max_length=300, blank=True)
+    tv3link = models.CharField(max_length=300, blank=True)
+    tv3ylink = models.CharField(max_length=300, blank=True)
 
-    tv4name = models.CharField(max_length=300,null=True, blank=True)
-    tv4link = models.CharField(max_length=300,null=True, blank=True)
-    tv4ylink = models.CharField(max_length=300,null=True, blank=True)
+    tv4name = models.CharField(max_length=300, blank=True)
+    tv4link = models.CharField(max_length=300, blank=True)
+    tv4ylink = models.CharField(max_length=300, blank=True)
 
-    tv5name = models.CharField(max_length=300,null=True, blank=True)
-    tv5link = models.CharField(max_length=300,null=True, blank=True)
-    tv5ylink = models.CharField(max_length=300,null=True, blank=True)
+    tv5name = models.CharField(max_length=300, blank=True)
+    tv5link = models.CharField(max_length=300, blank=True)
+    tv5ylink = models.CharField(max_length=300, blank=True)
 
     def __str__(self):
         """To String Method"""
@@ -31,8 +31,8 @@ class TvLink(models.Model):
 
 class AdsService(models.Model):
     """Database model for Ads Service"""
-    selectedAddService = models.CharField(max_length=20,null=True, blank=True)
-    status = models.CharField(max_length=10, null=True, blank=True)
+    selectedAddService = models.CharField(max_length=20, blank=True)
+    status = models.CharField(max_length=10, blank=True)
     cnt = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
