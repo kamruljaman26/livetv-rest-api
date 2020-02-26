3ed5550f25bc094ea2b209e2752054170d0a9206
kamruljaman26 <kamrul.jaman26@gmail.com>
2020-02-26 20:06:32 +0600

    Not Working

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -5,7 +5,6 @@ from profiles_api import models
 admin.site.register(models.TvLink) # General Tv Link
 admin.site.register(models.PslTvLink) # PSL TV Links
 admin.site.register(models.AdsService) # Ads Service Data
-
 admin.site.register(models.MatchSchedule) # PSL SCHEDULE
 admin.site.register(models.IplMatchSchedule) #IPL SCHEDULE
 admin.site.register(models.MatchNews) # ALL SPORTS NEWS
